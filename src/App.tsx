42c43000197120817762dbacc632af25133e56b6
David Hannah <dhann013@ucr.edu>
2025-08-18 17:39:27 -0500

    Fix clipping, increase hero responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import Background from "/landing/background.jpg";
-// import Logo from '/landing/logo-white.png';
-import NavBar from "./components/NavBar/NavBar";
-import CallToAction from "./components/CallToAction/CallToAction";
-import Curve from "./components/Curve/Curve";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
-    <div className="h-screen bg-black">
-      <div
-        style={{
-          backgroundImage: `
-            linear-gradient(
-              180deg, 
-              rgba(0, 0, 0, 0.85) 0%, 
-              rgba(0, 0, 0, 0.7) 50%,
-              rgba(0, 0, 0, 0.65) 100%
-            ),
-            url(${Background})
-          `,
-          height: "max(80vh, 45rem)",
-        }}
-        className="before: hero-bg-container bg-cover bg-center w-full"
-      >
-        <div
-          className={`hero-container flex flex-col justify-start text-center relative w-full h-full py-2 px-4 md:px-32 lg:px-64`}
-        >
-          <NavBar />
-          <CallToAction />
-          <Curve />
-        </div>
+    <div className="min-h-screen bg-black">
+      <Hero />
+      <div className="product-content w-full h-96 text-white">
+        Product Content
       </div>
-      <div className="product-content text-white">Product Content</div>
     </div>
   );
 }
--- src/components/Curve/Curve.tsx
+++ src/components/Curve/Curve.tsx
@@ -2,8 +2,14 @@ import Line from "../../assets/line.svg?react";
 
 export default function Curve() {
   return (
-    <div className="before: curve-container absolute left-0 bottom-0 w-full h-24 overflow-hidden">
-      <Line width="100%" height="200%" fill="black" />
+    <div className="curve-container absolute left-0 bottom-0 w-full h-32 overflow-visible">
+      <Line
+        width="100%"
+        height="100%"
+        fill="black"
+        className="relative"
+        preserveAspectRatio="none"
+      />
     </div>
   );
 }
