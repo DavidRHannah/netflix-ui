d058e804710ac406fcbb4929ba392d406f352053
David Hannah <165863032+DavidRHannah@users.noreply.github.com>
2025-08-18 18:22:44 -0500

    Beautify curve content separator
    * Add beautiful radial gradient to curve
    
    * Remove unnecessary fill attribute

--- src/components/Curve/Curve.tsx
+++ src/components/Curve/Curve.tsx
@@ -6,7 +6,6 @@ export default function Curve() {
       <Line
         width="100%"
         height="100%"
-        fill="black"
         className="relative"
         preserveAspectRatio="none"
       />
