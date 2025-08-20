91bbe0a9dbe132cb8dc9b8878a4c6b5968df4f5f
David Hannah <dhann013@ucr.edu>
2025-08-20 18:26:25 -0500

    Add responsive footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import AdBanner from "./components/AdBanner/AdBanner";
 import CallToActionInput from "./components/CallToAction/CallToActionInput";
 import Faq from "./components/Faq/Faq";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import ReasonsToJoin from "./components/ReasonsToJoin/ReasonsToJoin";
 import TrendingMovies from "./components/TrendingMovies/TrendingMovies";
 
 function App() {
   return (
-    <div className="bg-black overflow-x-hidden">
+    <div className="bg-black flex flex-col items-center overflow-x-hidden">
       <Hero />
       <div className="product-content flex flex-col items-center w-full mt-2 ml-2 h-full">
         <AdBanner />
         <TrendingMovies />
         <ReasonsToJoin />
         <Faq />
+        <CallToActionInput />
       </div>
-      <CallToActionInput />
+      <Footer />
+      <div className="empty-space h-36"></div>
     </div>
   );
 }
